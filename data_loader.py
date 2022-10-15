5d60f9d6acfd47323a422686315ffc8c048e2f04
ahmad12hamdan99 <ahmad12hamdan99@gmail.com>
2022-10-16 00:48:03 +0300

    data_loader

--- data_loader.py
+++ data_loader.py
@@ -22,31 +22,23 @@ class DriveAPI:
 
     def __init__(self):
 		
-		# Variable self.creds will
-		# store the user access token.
-		# If no valid token found
-		# we will create one.
+		# Variable self.creds will store the user access token. If no valid token found we will create one.
         self.creds = None
         folder_ID = '1a-ImiHIDaye9kpACvyi0ysdqHNcoj8bW'
-		# The file token.pickle stores the
-		# user's access and refresh tokens. It is
-		# created automatically when the authorization
-		# flow completes for the first time.
+		# The file token.pickle stores the user's access and refresh tokens. It is
+		# created automatically when the authorization flow completes for the first time.
 
 		# Check if file token.pickle exists
         if os.path.exists('token.pickle'):
 
-			# Read the token from the file and
-			# store it in the variable self.creds
+			# Read the token from the file and store it in the variable self.creds
             with open('token.pickle', 'rb') as token:
                 self.creds = pickle.load(token)
 
-		# If no valid credentials are available,
-		# request the user to log in.
+		# If no valid credentials are available, request the user to log in.
         if not self.creds or not self.creds.valid:
 
-			# If token is expired, it will be refreshed,
-			# else, we will request a new one.
+			# If token is expired, it will be refreshed, else, we will request a new one.
             if self.creds and self.creds.expired and self.creds.refresh_token:
                 self.creds.refresh(Request())
             else:
@@ -54,31 +46,42 @@ class DriveAPI:
                     'credentials.json', SCOPES)
                 self.creds = flow.run_local_server(port=0)
 
-			# Save the access token in token.pickle
-			# file for future usage
+			# Save the access token in token.pickle file for future usage
             with open('token.pickle', 'wb') as token:
                 pickle.dump(self.creds, token)
 
 		# Connect to the API service
         self.service = build('drive', 'v3', credentials=self.creds)
 
-        # request a list of first N files or
-        # folders with name and id from the API.
+        # request a list of first N files or folders with name and id from the API.
         page_token = None
         # q="parents in '"+ folder_ID +"'",
         results = self.service.files().list(  q="parents in '"+ folder_ID +"'",
                                               spaces='drive',
             pageSize=100, fields='files(id, name)',).execute()
-        items = results.get('files', [])
+        self.items = results.get('files', [])
 
 		# print a list of files
-        for i in items:
-            print(i['id'])
+        for i in self.items:
+            print(i['name'])
         print('------------')
         print("Here's a list of files: \n")
-        print(*items, sep="\n", end="\n\n")
+        print(*self.items, sep="\n", end="\n\n")
 
-    def FileDownload(self, file_id, file_name):
+    def search_for_id(self, name):
+        print('looking for file_id')
+        #change the name: 
+        name = 'raw_' + name + '.csv'
+        for i in self.items:
+            if i['name'] == name:
+                print('found it!!!')
+                return i['id']
+
+        return 'Error: file_id not found'
+
+    def FileDownload(self, name, file_name):
+        #get the file_id 
+        file_id = self.search_for_id(name)
         request = self.service.files().get_media(fileId=file_id)
         fh = io.BytesIO()
     
@@ -135,15 +138,5 @@ if __name__ == "__main__":
 	obj = DriveAPI()
 	i = int(input('''Enter your choice:
 				1 - Download file, 2- Upload File, 3- Exit.\n'''))
-	
-	if i == 1:
-		f_id = input("Enter file id: ")
-		f_name = input("Enter file name: ")
-		obj.FileDownload(f_id, f_name)
-		
-	elif i == 2:
-		f_path = input("Enter full file path: ")
-		obj.FileUpload(f_path)
-	
-	else:
-		exit()
+
+	obj.FileDownload('raw_2022_09_07.csv', 'downloadedfromdrive')
